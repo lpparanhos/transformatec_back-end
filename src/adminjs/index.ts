fb64b8bef6be39986fce221f7f9cc7432a0b33f3
lpparanhos <paranhos.pan@gmail.com>
2022-10-21 12:10:39 -0300

    2110221210

---
+++ src/adminjs/index.ts
@@ -0,0 +1,35 @@
+import AdminJs from "adminjs"
+import AdminJsExpress from "@adminjs/express"
+import AdminJsSequelize from "@adminjs/sequelize"
+import { database } from "../database"
+
+AdminJs.registerAdapter(AdminJsSequelize)
+
+export const adminJs = new AdminJs({
+    databases: [database],
+    rootPath: "/admin",
+    branding: {
+        companyName: 'TransformaTec',
+        logo: '/logo-transforma-tec.svg',
+        theme: {
+            colors: {
+                primary100: '#f06c28',
+                primary80: '#f08c28',
+                primary60: '#f05c28',
+                primary40: '#f04c28',
+                primary20: '#f07c28',
+                grey100: '#151515',
+                grey80: '#333333',
+                grey60: '#4d4d4d',
+                grey40: '#666666',
+                grey20: '#dddddd',
+                filterBg: '#333333',
+                accent: '#151515',
+                hoverBg: '#151515',
+            }
+        }
+
+    }
+})
+
+export const adminJsRouter = AdminJsExpress.buildRouter(adminJs)
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import {adminJs, adminJsRouter} from "./adminjs";
 import { database } from "./database";
 
 const app = express();
 
+app.use(adminJs.options.rootPath, adminJsRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen (PORT, () => {
-    database.authenticate().then(() => console.log('DB connection successfully'))
+    database.authenticate().then(() => console.log('DB connection successfully'));
     console.log(`Server started successfully at port ${PORT}`);
 });
