c942287438c1010fd35406b0814eb242ab904e2c
lpparanhos <paranhos.pan@gmail.com>
2022-10-23 14:00:53 -0300

    2310221400 - buscando por cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { getPaginationParams } from '../helpers/getPaginationParams'
 import { courseService } from '../services/courseService'
 
 export const coursesController = {
@@ -17,6 +18,7 @@ export const coursesController = {
 
     // GET /courses/newest
     newest: async (req: Request, res: Response) => {
+
         try {
             const newestCourses = await courseService.getTopTenNewest()
             return res.json(newestCourses)
@@ -29,11 +31,27 @@ export const coursesController = {
 
     // GET /courses/:id
     show: async (req: Request, res: Response) => {
-
         const { id } = req.params
+
         try {
             const course = await courseService.findByIdWithEpisodes(id)
             return res.json(course)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // GET /courses/search?name=
+    search: async (req: Request, res: Response) => {
+        const { name } = req.query
+        const [page, perPage] = getPaginationParams(req.query)
+
+        try {
+            if (typeof name !== 'string') throw new Error('name param must be of type string');
+            const courses = await courseService.findByName(name, page, perPage)
+            return res.json(courses)
         } catch (err) {
             if (err instanceof Error) {
                 return res.status(400).json({ message: err.message })
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,3 +1,4 @@
+import { Op } from "sequelize"
 import { Course } from "../models"
 
 export const courseService = {
@@ -29,13 +30,17 @@ export const courseService = {
 
     getRandomFeaturedCourses: async () => {
         const featuredCourses = await Course.findAll({
-            attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']],
+            attributes: [
+                'id',
+                'name',
+                'synopsis',
+                ['thumbnail_url', 'thumbnailUrl']],
             where: {
                 featured: true
             }
         })
         const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
-        
+
         return randomFeaturedCourses.slice(0, 3)
     },
 
@@ -46,5 +51,30 @@ export const courseService = {
         })
 
         return courses
+    },
+
+    findByName: async (name: string, page: number, perPage: number) => {
+        const offset = (page - 1) * perPage
+        const { count, rows } = await Course.findAndCountAll({
+            attributes: [
+                'id',
+                'name',
+                'synopsis',
+                ['thumbnail_url', 'thumbnailUrl']],
+            where: {
+                name: {
+                    [Op.iLike]: `%${name}%`
+                }
+            },
+            limit: perPage,
+            offset
+        })
+
+        return {
+            courses: rows,
+            page,
+            perPage,
+            total: count
+        }
     }
 }
