e888256f4b1961c68a4db256fa9ac6a28411b818
lpparanhos <paranhos.pan@gmail.com>
2022-10-23 16:15:32 -0300

    2310221615 - registro de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesController'
@@ -11,6 +12,8 @@ router.get('/courses/featured', coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
+
+router.post('/auth/register', authController.register)
 
 router.get('/episodes/stream', episodesController.stream)
 
